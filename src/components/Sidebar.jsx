d1d81e9e32e21ba3266117111bcd75b45c8ff554
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-24 22:13:56 +0200

    Fix text on dark mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ import { useStateContext } from '../contexts/ContextProvider'
 const Sidebar = () => {
   const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-gray-200 m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md dark:text-gray-200 dark:hover:text-black hover:bg-gray-200 m-2'
   const handleCloseSideBar = () => {
     if (activeMenu && screenSize <= 900 ) {
       setActiveMenu(false)
@@ -19,7 +19,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={handleCloseSideBar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to="/" onClick={handleCloseSideBar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white'>
             <SiShopware /><span>Grow</span>
           </Link>
           <TooltipComponent content={'Menu'} position='bottomCenter'>
